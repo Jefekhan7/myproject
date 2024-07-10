4d70a79b13f78ea0f9cc20a67c449757d026c498
= <yt057668@gmail.com>
2024-07-10 15:47:00 +0500

    new changes made

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ window.onload = function () {
 
 let bgAudio = document.getElementById("background-audio");
 
-
 const Days = document.querySelector("#days");
 const Hours = document.querySelector("#hours");
 const Minutes = document.querySelector("#minutes");
@@ -17,6 +16,27 @@ const Seconds = document.querySelector("#seconds");
 
 var IntervalId;
 let video = document.querySelector("#background-video");
+let audio = document.querySelector("#background-audio");
+
+let isAudioplaying = false;
+document.addEventListener('touchstart',() => {
+ if(isAudioplaying) {
+  audio.pause();
+ }else {
+   audio.play();
+}
+ 
+isAudioplaying = !isAudioplaying;
+});
+document.addEventListener('click',() => {
+ if(isAudioplaying) {
+  audio.pause();
+ }else {
+   audio.play();
+}
+ 
+isAudioplaying = !isAudioplaying;
+});
 
 function Calculate() {
   let currentDate = new Date();
@@ -32,15 +52,61 @@ function CalculateTime(endTime) {
   if (endTime > currentTime) {
     console.log(timeLeft);
 
-    if (timeLeft >= 0 && timeLeft <= 11) {
+    if (timeLeft <= 11) {
+      clearInterval(IntervalId);
+      document.removeEventListener('touchstart',() => {
+        if(isAudioplaying) {
+         audio.pause();
+        }else {
+          audio.play();
+       }
+        
+       isAudioplaying = !isAudioplaying;
+       });
+       document.removeEventListener('click',() => {
+        if(isAudioplaying) {
+         audio.pause();
+        }else {
+          audio.play();
+       }
+        
+       isAudioplaying = !isAudioplaying;
+       });
       let div = document.querySelector(".main-container").classList.add("hide");
       video.currentTime = 112;
       video.currentTime++;
+      //****** *
+      var btn = document.getElementById('count-down');
+      btn.classList.remove('hide');
+      var msg = document.getElementById('show-tensecond');
+
       
+      let tensecondaudio = document.querySelector("#countaudio");
+   
+      btn.addEventListener('click',() => {
+        var seconds = 11;
+        tensecondaudio.play();
+        btn.classList.add('hide');
+        msg.classList.remove('hide');
+        function tick() {
+            seconds--;
+            msg.innerHTML = seconds;
+            console.log(seconds);
+            if (seconds > 0) {
+                setTimeout(tick, 1150); // Call tick again after 1 second
+            } else {
+                console.log('Countdown complete!');
+                // Here you can perform any actions you want after countdown finishes
+            }
+        }
+        tick(); // Start the countdown
+      })
     }
-    
-    if(timeLeft <= 0) {
-      let element = document.querySelector("#background-video").classList.add("hide");
+
+    if (timeLeft <= 0) {
+      let element = document
+        .querySelector("#background-video")
+        .classList.add("hide");
     }
     Days.innerHTML = Math.floor(timeLeft / (24 * 60 * 60));
     Hours.innerHTML = Math.floor((timeLeft / (60 * 60)) % 24);
@@ -54,3 +120,5 @@ function CalculateTime(endTime) {
     Seconds.innerHTML = 0;
   }
 }
+
+
